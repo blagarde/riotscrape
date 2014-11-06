d8f0495346607c65d6d9fc97811e5b3a6a6c2c08
Baptiste Lagarde <lagarde.baptiste@gmail.com>
2014-11-06 17:57:52 +0100

    ADD: initial commit

---
+++ main.py
@@ -0,0 +1,107 @@
+# -*- coding: utf-8 -*-
+import sys
+from collections import deque
+from itertools import cycle
+from time import sleep
+from config import KEYS
+from riotwatcher.riotwatcher import RiotWatcher, EUROPE_WEST, LoLException
+
+
+USERS_FILE = 'users.txt'
+GAMES_FILE = 'games.txt'
+
+
+class EmptyUserQueue(IndexError):
+    pass
+
+class EmptyGameQueue(IndexError):
+    pass
+
+class UncaughtException(RuntimeError):
+    pass
+
+
+def squelch_errors(method):
+    '''
+    Decorator to squelch most errors.
+    Just output them to stderr
+    '''
+    def raising(fn):
+        def run(*args):
+            try:
+                return fn(*args)
+            except (EmptyGameQueue, EmptyUserQueue) as e:
+                raise e
+            except Exception as e:
+                tpl = (fn.__name__, e.__class__.__name__, str(e))
+                sys.stderr.write("Exception squelched inside '%s': %s (%s)" % tpl)
+                sys.stderr.flush()
+                pass
+        return run
+    return raising(method)
+
+
+class Scraper(object):
+    games = set()
+    users = set()
+    uq = deque()
+    gq = deque()
+    def __init__(self):
+        WATCHERS = [RiotWatcher(k) for k in KEYS]
+        self.watchers = cycle(WATCHERS)
+        self.watcher = next(self.watchers)
+        print("API KEY STATUS")
+        for w in WATCHERS:
+            print("%s\t%s" % (w.key, w.can_make_request()))
+
+        challengers = self.get_challengers()
+        self.uq += [c for c in challengers if c not in self.users]
+
+    def scrape(self):
+        while True:
+            while not self.watcher.can_make_request():
+                self.watcher = next(self.watchers)
+                sleep(0.001)
+            try:
+                self.do_game()
+            except EmptyGameQueue:
+                try:
+                    self.do_user()
+                except EmptyUserQueue:
+                    continue
+            except LoLException:
+                sleep(10)
+    
+    def get_challengers(self):
+        league_dct = self.watcher.get_challenger(region=EUROPE_WEST)
+        return [e['playerOrTeamId'] for e in league_dct['entries']]
+
+    @squelch_errors
+    def do_user(self):
+        try:
+            userid = self.uq.popleft()
+        except IndexError as e:
+            raise EmptyUserQueue(e)
+        games = self.watcher.get_recent_games(userid, region=EUROPE_WEST)['games']
+        for game_dct in games:
+            self.uq += [dct['summonerId'] for dct in game_dct['fellowPlayers'] if dct['summonerId'] not in self.users]
+            if game_dct['gameId'] not in self.games:
+                self.gq += [game_dct['gameId']]
+        self.users |= set([userid])
+    
+    @squelch_errors
+    def do_game(self):
+        try:
+            gameid = self.gq.popleft()
+        except IndexError as e:
+            raise EmptyGameQueue(e)
+        dumpme = self.watcher.get_match(gameid, region=EUROPE_WEST, include_timeline=True)
+        #self.es.index(dumpme)
+        print(gameid)
+        self.uq += [dct['player']['summonerId'] for dct in dumpme['participantIdentities'] if dct['player']['summonerId'] not in self.users]
+        self.games |= set([gameid])
+
+
+if __name__ == "__main__":
+    s = Scraper()
+    s.scrape()
