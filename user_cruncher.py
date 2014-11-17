13457b4ff581658709c804c9886aea476ed3aab8
willpa <wp@little-big-data.com>
2014-11-17 11:50:33 +0100

    ADD:  UserCruncher uses games.txt and users.txt, iterates over games id, keeping users in memory

--- user_cruncher.py
+++ user_cruncher.py
@@ -2,36 +2,50 @@ from elasticsearch import helpers, Elasticsearch
 from config import ES_NODES, RIOT_INDEX, GAME_DOCTYPE, USER_DOCTYPE
 from feature_extractor import FeatureExtractor
 from user import User
+from collections import defaultdict
+
 
 class UserCruncher(object):
     ES = Elasticsearch(ES_NODES)
     FE = [FeatureExtractor]
     USERS_ID = set()
+    GAMES_ID = set()
+    USERS = defaultdict(User)
     with open('user_games.txt', 'r') as fh:
         for line in fh:
-            user_id, _, _ = line.split("\t")
-            USERS_ID.add(user_id)
+            USERS_ID.add(int(line))
+    with open('games.txt', 'r') as f:
+        for line in f:
+            GAMES_ID.add(int(line))
 
     def extract_games(self, user_id):
-        scan = helpers.scan(client=self.ES, query=self.write_query(user_id), scroll="5m", index=RIOT_INDEX, doc_type=GAME_DOCTYPE)
+        #TODO: this method is no longer useful
+        scan = helpers.scan(client=self.ES, query=self.write_query(user_id),
+                            scroll="5m", index=RIOT_INDEX, doc_type=GAME_DOCTYPE)
         for game in scan:
             yield game['_source']
-
-    def insert_user(self, user):
-        self.ES.index(RIOT_INDEX, doc_type=USER_DOCTYPE, body=user)
-
-    def get_user(self,user_id):
-        res = self.ES.get(index=RIOT_INDEX, doc_type=USER_DOCTYPE, id=user_id)
-        return User(res) 
-
-    def process(self):
-        for user_id in self.USERS_ID:
-            user = self.get_user(user_id)
-            games = list(self.extract_games(user_id))
-            for f in self.FE:
-                user = f.apply(user, games)
-            self.insert_user(user)
 
     @staticmethod
     def write_query(user_id):
         return {'query': {'filtered': {'filter': {'term': {"participantIdentities.player.summonerId": user_id}}}}}
+
+    def get_game(self, game_id):
+        return self.ES.get(index=RIOT_INDEX, doc_type=GAME_DOCTYPE, id=game_id)
+
+    def get_user(self, user_id):
+        res = self.ES.get(index=RIOT_INDEX, doc_type=USER_DOCTYPE, id=user_id)
+        return User(res)
+
+    def insert_user(self, user):
+        self.ES.index(RIOT_INDEX, doc_type=USER_DOCTYPE, body=user)
+
+    def process(self):
+        for game_id in self.GAMES_ID:
+            game = self.get_game(game_id)
+            for participant in game["_source"]["participantIdentities"]:
+                if "summonerId" not in participant:
+                    continue
+                user_id = participant["summonerId"]
+                for f in self.FE:
+                    self.USERS[user_id] = f.apply(self.USERS[user_id], game
+            #TODO: write 
