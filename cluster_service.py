5b2297bd9a549eea5842d0d5a645b7fd610dff68
Thibaut Fatus <tf@little-big-data.com>
2014-12-03 14:37:01 +0100

    ok

--- cluster_service.py
+++ cluster_service.py
@@ -6,17 +6,15 @@ class ClusterService(object):
     
     def __init__(self, scaler_fn, clf_fn):
         self.translator = RituTranslator()
-        self.scaler = StdScaler().load(scaler_fn)
+        self.scaler = StdScaler()
+        self.scaler.load(scaler_fn)
         self.clf = SkLabelling(clf_file=clf_fn)
 
     def get_user_cluster(self, user):
         u_v = self.translator.translate(user)
-        print u_v
         if len(u_v) > 0:
             u_s = self.scaler.transform(u_v)
-            print u_s
-            label = self.clf.get_label(u_s)
-            print label
+            label = int(self.clf.get_label(u_s)[0])
             return label
         else:
             print "Error: unable to translate user"
--- web_service.py
+++ web_service.py
@@ -1,6 +1,7 @@
 from bottle import route, run, template, request
 from user_service import UserService
 from cluster_service import ClusterService
+import json
 
 ## these should go to config once we know where to store / how to store them
 scaler_file = "labelling_files/scaler_9g"
@@ -12,8 +13,16 @@ cs = ClusterService(scaler_file, clf_file)
 @route('/userservice/<region>/<summonername>')
 def get_user_data(region,summonername):
     res = us.get_crunched_user(summonername, region)
-    if res[0] == 200:
-        cs.get_user_cluster(res[1])
-    return res[1]
+    if res[0] == '200':
+        ret = dict()
+        ret["user"] = res[1]
+        arg = dict()
+        arg["_source"] = res[1]
+        ret["label"] = cs.get_user_cluster(arg)
+        print ret
+        retjs = json.dumps(ret, separators=(',',':'))
+        return retjs
+    else:
+        return "Error not found %s" % res[1]
 
 run(host='localhost', port=8080)
