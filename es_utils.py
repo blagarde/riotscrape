48dbd38ba969331d65815b86ae47a9cc601536a6
Baptiste Lagarde <lagarde.baptiste@gmail.com>
2014-12-09 13:55:21 +0100

    Try this (bulk now casts to list)

--- es_utils.py
+++ es_utils.py
@@ -15,4 +15,5 @@ def bulk_action_gen(items, index, doctype, id_fieldname='id'):
 
 def bulk_upsert(client, index, doctype, items, chunk_size=100, id_fieldname='id'):
     action_gen = bulk_action_gen(items, index, doctype, id_fieldname=id_fieldname)
-    helpers.bulk(client=client, actions=action_gen, chunk_size=chunk_size)
+    lst = list(action_gen)
+    helpers.bulk(client=client, actions=lst, chunk_size=min(len(lst), chunk_size))
