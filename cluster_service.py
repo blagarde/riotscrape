b38a425945b260c22a840eb8aa4385b055d23b31
Thibaut Fatus <tf@little-big-data.com>
2014-12-10 17:45:16 +0100

    doc

--- cluster_service.py
+++ cluster_service.py
@@ -4,13 +4,28 @@ from learner.translator.riot_hl_translator import RiotHLTranslator
 import pickle
 
 class ClusterService(object):
+    '''
+    This class aims to compute high level features from json user, 
+    and then apply clustering pipeline to compute the corresponding label
+
+    scaler_fn and clf_fn are the scaler pickle and the classifier pickle 
+    written when clustering_pipe.write is called
+
+    TODO we should be able to use a config file to load all steps of clustering_pipeline
+    '''
     
     def __init__(self, scaler_fn='labelling_files/scaler.pkl', clf_fn='labelling_files/clf.pkl'):
         self.translator = RiotHLTranslator()
-        self.scaler = self.load(scaler_fn)
+        self.scaler = self._load(scaler_fn)
         self.clf = SkLabelling(clf_file=clf_fn)
 
     def get_user_cluster(self, user):
+        '''
+        take a user as json (like the ones from ES)
+
+        returns the computed label (int)
+        if there is an error (unable to translate, or to label) returns -1
+        '''
         u_v = self.translator.transform(user)
         if len(u_v) > 0:
             u_s = self.scaler.transform(u_v)
@@ -20,6 +35,6 @@ class ClusterService(object):
             print "Error: unable to translate user"
             return -1
     
-    def load(self, fn):
+    def _load(self, fn):
         with open(fn, 'rb') as f:
             return pickle.load(f)
