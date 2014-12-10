8a02dc5309814e1822096f40a733283e8b773b58
Thibaut Fatus <tf@little-big-data.com>
2014-12-10 11:18:48 +0100

    go UB ! (fix refactor)

--- cluster_service.py
+++ cluster_service.py
@@ -1,17 +1,17 @@
-from cluster.preprocessing.std_scaler import StdScaler
-from cluster.labelling.sk_labelling import SkLabelling
-from cluster.translator.ritu_translator import RituTranslator
+from sklearn.preprocessing import StandardScaler
+from learner.labelling.sk_labelling import SkLabelling
+from learner.translator.riot_hl_translator import RiotHLTranslator
+import pickle
 
 class ClusterService(object):
     
     def __init__(self, scaler_fn, clf_fn):
-        self.translator = RituTranslator()
-        self.scaler = StdScaler()
-        self.scaler.load(scaler_fn)
+        self.translator = RiotHLTranslator()
+        self.scaler = self.load(scaler_fn)
         self.clf = SkLabelling(clf_file=clf_fn)
 
     def get_user_cluster(self, user):
-        u_v = self.translator.translate(user)
+        u_v = self.translator.transform(user)
         print u_v
         if len(u_v) > 0:
             u_s = self.scaler.transform(u_v)
@@ -21,3 +21,8 @@ class ClusterService(object):
         else:
             print "Error: unable to translate user"
             return -1
+    
+    def load(self, fn):
+        print fn
+        with open(fn, 'rb') as f:
+            return pickle.load(f)
