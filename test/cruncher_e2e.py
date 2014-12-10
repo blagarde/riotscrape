1bce5080fd9bacd7354971f4a17e8669a931a2af
willpa <wp@little-big-data.com>
2014-12-10 15:13:09 +0100

    TEST : update important

--- test/cruncher_e2e.py
+++ test/cruncher_e2e.py
@@ -2,7 +2,7 @@ import unittest
 from init_redis import init_redis_for_testing_cruncher
 from init_elasticsearch import init_elasticsearch_for_testing_cruncher, delete_elasticsearch_index
 from crunchers import GameCruncher
-from config import RIOT_GAMES_INDEX, RIOT_USERS_INDEX, ES_NODES
+from config import RIOT_GAMES_INDEX, RIOT_USERS_INDEX, ES_NODES, TESTING
 from elasticsearch import Elasticsearch
 from time import sleep
 
@@ -10,13 +10,20 @@ from time import sleep
 class CruncherTests(unittest.TestCase):
 
     def setUp(self):
-        print "Set up Redis and Elasticsearch"
+        '''
+        Set up Elasticsearch and Redis
+        '''
+        if not TESTING:
+            raise EnvironmentError("You are not in testing mode, you could have deleted the prod instances!")
+        print "WARNING : the script update_agg_data.py needs to be in the config/scripts folder of your local ES instance or cruncher tests will fail"
         init_redis_for_testing_cruncher("game_id_sample.txt", "user_sample.txt")
         init_elasticsearch_for_testing_cruncher()
         sleep(1)
 
     def test_from_redis_to_elasticsearch(self):
-        print "Crunch mock data"
+        '''
+        Crunch mock data and then retrieve crunched users
+        '''
         gc = GameCruncher()
         gc.crunch()
         sleep(2)
@@ -26,6 +33,9 @@ class CruncherTests(unittest.TestCase):
         self.assertEqual(10, nb_user_crunched['count'])
 
     def tearDown(self):
+        '''
+        Clean Elasticsearch instance
+        '''
         delete_elasticsearch_index(RIOT_GAMES_INDEX)
         delete_elasticsearch_index(RIOT_USERS_INDEX)
 
