be5475fd5241f7cfc2c217aeeed85bfdefbe330d
Ulysse Boillot <ub@little-big-data.com>
2014-12-11 15:35:34 +0100

    webservice fix for prod

--- web_service.py
+++ web_service.py
@@ -10,16 +10,19 @@ clf_file = "labelling_files/clf.pkl"
 us = UserService()
 cs = ClusterService(scaler_file, clf_file)
 
-@route('/userservice/<region>/<summonername>')
-def get_user_data(region,summonername):
+c = [[float(i)/10, float(i+1)/10] for i in range(10)]
+@route('/userservice/<region>/<summonername>/<feature>')
+def get_user_data(region,summonername,feature):
     res = us.get_crunched_user(summonername, region)
     if res[0] == '200':
         ret = dict()
-        ret["user"] = res[1]
+        usr = res[1]
         ret["label"] = cs.get_user_cluster(res[1])
+        ret["features"] = [gen_feature_bar({ "name":f[0], "choices":c, "index":i }, ret["user"], ret["label"]) for i,f in enumerate(CLUSTER_DATA[ret["label"]]["top"])]
+        ret["graph"] = gen_histo(feature, ret["user"], ret["label"], {"and":[{"label":[ret["label"]]}], "or":[], "not":[]}, BLANK_FILTERS)
         retjs = json.dumps(ret, separators=(',',':'))
         return retjs
     else:
         return "Error not found %s" % res[1]
 
-run(host='localhost', port=8080)
+run(host='0.0.0.0', port=8080)
