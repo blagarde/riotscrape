ed63f0c54c4c31c1fd40aac721b37c2740e5c686
Ulysse Boillot <ub@little-big-data.com>
2014-12-12 11:04:43 +0100

    cr

--- brief.py
+++ brief.py
@@ -1,146 +1,13 @@
 from multiprocessing import Pool
 from elasticsearch.client import Elasticsearch
-from config import ES_NODES, BLANK_FILTERS
+from config import ES_NODES, BLANK_FILTERS, RIOT_USERS_INDEX
 from copy import deepcopy
+from cluster_data import CLUSTER_DATA
 
-
-CLUSTER_DATA = {
-        0: {
-            'count': 19314,
-            'top': [
-                ('role',
-                0.5250400539890705,
-                0.15503665010391465),
-                ('solo',
-                0.5728517060761398,
-                0.15167097920733186),
-                ('loyalty',
-                0.2624550089086491,
-                0.113467147344253),
-                ('teamplay',
-                0.315978534244557,
-                -0.10469925656239937),
-                ('position',
-                0.4035167229234775,
-                0.03800076865933871),
-                ('strategy',
-                0.22818659296544017,
-                -0.012370542132689427),
-                ('action',
-                0.3290872741509492,
-                0.005114788130947567)
-            ]
-        },
-        1: {
-            'count': 32581,
-            'top': [
-                ('solo',
-                0.3069566023768998,
-                -0.11422412449190811),
-                ('position',
-                0.4754761555633881,
-                0.10996020129924933),
-                ('teamplay',
-                0.504724412959805,
-                0.08404662215284858),
-                ('action',
-                0.29011932808563173,
-                -0.03385315793436988),
-                ('strategy',
-                0.2722542407654162,
-                0.03169710566728662),
-                ('loyalty',
-                0.1785755337646831,
-                0.02958767220028702),
-                ('role',
-                0.3777979150991183,
-                0.007794511213962463)
-            ]
-        },
-        2: {
-            'count': 70481,
-            'top': [
-                ('role',
-                0.30779551393885074,
-                -0.062207889946305106),
-                ('position',
-                0.32368777107428287,
-                -0.04182818318985593),
-                ('loyalty',
-                0.11418326228437363,
-                -0.034804599280022455),
-                ('solo',
-                0.3955936562592702,
-                -0.025587070609537743),
-                ('teamplay',
-                0.4414749248175933,
-                0.02079713401063693),
-                ('strategy',
-                0.24905612195507912,
-                0.008498986856949525),
-                ('action',
-                0.32224013216396,
-                -0.0017323538560415908)
-            ]
-        },
-        3: {
-            'count': 69071,
-            'top': [
-                ('position',
-                0.297169494056667,
-                -0.06834646020747182),
-                ('solo',
-                0.48160468085175384,
-                0.06042395398294592),
-                ('teamplay',
-                0.37240318353173474,
-                -0.04827460727522165),
-                ('loyalty',
-                0.11495852738465474,
-                -0.03402933417974134),
-                ('strategy',
-                0.21422586133142854,
-                -0.026331273766701052),
-                ('action',
-                0.3473686477596499,
-                0.023396161739648258),
-                ('role',
-                0.35653235908076997,
-                -0.013471044804385879)
-            ]
-        },
-        4: {
-            'count': 8553,
-            'top': [
-                ('position',
-                0.7574591751192588,
-                0.39194322085511996),
-                ('role',
-                0.6116264597737138,
-                0.24162305588855792),
-                ('loyalty',
-                0.34166910454555155,
-                0.19268124298115546),
-                ('solo',
-                0.2366866669758876,
-                -0.1844940598929203),
-                ('teamplay',
-                0.5554154316098552,
-                0.1347376408028988),
-                ('action',
-                0.2667159053240329,
-                -0.05725658069596873),
-                ('strategy',
-                0.29035388111463156,
-                0.04979674601650197)
-            ]
-        }
-    }
 
 def _get_elt(elt):
     res = []
     for key, val in elt.items():
-    # for key, val in elt:
         if key in ["and", "or", "not"]:
             for logic, eslogic in {"and":"must", "or":"should", "not":"must_not"}.items():
                 if logic == key:
@@ -150,7 +17,7 @@ def _get_elt(elt):
                     res.append(o)
         else:
             if key == "label":
-                res.append({ "term": {key:val} })
+                res.append({ "term": { key: val } })
             else:
                 res.append({'range':{ ("feature."+str(key)):{"gte":val[0],"lte":val[1]}}})
     return res
@@ -173,7 +40,7 @@ def count(flt):
     req = init_query()
     write_filters_in_request(req, flt)
     es = Elasticsearch(ES_NODES)
-    res = es.count(index="lagrosserita", body=req)['count']
+    res = es.count(index=RIOT_USERS_INDEX, body=req)['count']
     return res
 
 
@@ -194,14 +61,11 @@ class Reader(object):
     It should be the only object that uses the DAO
     '''
     pool = Pool(processes=8)
-    def get_nb_of(self, requests, aud="undefined"):
+    def get_nb_of(self, requests):
         '''
         get_nb_of count a population which fits some filters
         '''
-        #res = parallelize(count, self.pool, requests)
-        res = []
-        for r in requests:
-            res.append(count(r))
+        res = parallelize(count, self.pool, requests)
         return res
 
 class Filters(dict):
@@ -263,22 +127,24 @@ class Brief(object):
 
 
 def gen_feature_bar(feature, user, cluster):
+    _, cluster_avg, genpop_delta = CLUSTER_DATA[cluster]["top"][feature["index"]]
     res = {
             "feature": feature["name"],
             "user": user["feature"][feature["name"]],
-            "cluster": CLUSTER_DATA[cluster]["top"][feature["index"]][1],
-            "genpop": CLUSTER_DATA[cluster]["top"][feature["index"]][1]-CLUSTER_DATA[cluster]["top"][feature["index"]][2]
+            "cluster": cluster_avg,
+            "genpop": cluster_avg - genpop_delta
         }
     return res
 
-"""
-    #feature: { "name": "n",
-                "choices": ["a","b","c"] }
-    #user: { "aggregate": { "a": "aa", "b": "bb" },
-             "feature": { "1": "11", "2": "22" } }
-    #cluster: "cluster_name"
-    #filters: { "and": [], "or": [], "not": [] }
-"""
+
 def gen_histo(feature, user, cluster, filters, comp_filters):
-    b = Brief(filters, { "key":feature, "choices": [[float(i)/10, float(i+1)/10] for i in range(10)], "format":"PCTG" }, comp_filters)
+    """
+        #feature: { "name": "n" }
+        #user: { "aggregate": { "a": "aa", "b": "bb" },
+                 "feature": { "1": "11", "2": "22" } }
+        #cluster: "cluster_name"
+        #filters: { "and": [], "or": [], "not": [] }
+    """
+    choices = [[float(i)/10, float(i+1)/10] for i in range(10)]
+    b = Brief(filters, { "key":feature, "choices": choices, "format":"PCTG" }, comp_filters)
     return b.to_json()
--- web_service.py
+++ web_service.py
@@ -2,6 +2,9 @@ from bottle import route, run, template, request
 from user_service import UserService
 from cluster_service import ClusterService
 import json
+from brief import gen_histo, gen_feature_bar
+from config import BLANK_FILTERS
+from cluster_data import CLUSTER_DATA
 
 ## these should go to config once we know where to store / how to store them
 scaler_file = "labelling_files/scaler.pkl"
@@ -10,19 +13,21 @@ clf_file = "labelling_files/clf.pkl"
 us = UserService()
 cs = ClusterService(scaler_file, clf_file)
 
-c = [[float(i)/10, float(i+1)/10] for i in range(10)]
-@route('/userservice/<region>/<summonername>/<feature>')
+@route("/userservice/<region>/<summonername>/<feature>")
 def get_user_data(region,summonername,feature):
     res = us.get_crunched_user(summonername, region)
-    if res[0] == '200':
+    if res[0] == "200":
         ret = dict()
         usr = res[1]
-        ret["label"] = cs.get_user_cluster(res[1])
-        ret["features"] = [gen_feature_bar({ "name":f[0], "choices":c, "index":i }, ret["user"], ret["label"]) for i,f in enumerate(CLUSTER_DATA[ret["label"]]["top"])]
-        ret["graph"] = gen_histo(feature, ret["user"], ret["label"], {"and":[{"label":[ret["label"]]}], "or":[], "not":[]}, BLANK_FILTERS)
-        retjs = json.dumps(ret, separators=(',',':'))
+        ret["label"] = cs.get_user_cluster(usr)
+        if ret["label"] == -1:
+            return "Sorry man"
+        ret["features"] = [gen_feature_bar({ "name":f[0], "index":i }, usr, ret["label"]) for i,f in enumerate(CLUSTER_DATA[ret["label"]]["top"])]
+        ret["graph"] = gen_histo(feature, usr, ret["label"], {"and":[{"cluster":ret["label"]}], "or":[], "not":[]}, BLANK_FILTERS)
+        retjs = json.dumps(ret, separators=(",",":"))
         return retjs
     else:
         return "Error not found %s" % res[1]
 
-run(host='0.0.0.0', port=8080)
+run(host="0.0.0.0", port=80)
+
